ef9137ba284422c2d33b9ff1c5b48ef7f8e8eeac
krishapajvani <krishapajvani15@gmail.com>
2026-03-10 23:14:09 +0530

    Refactor user signup route with registration logic
    Refactor signup route to include user registration logic and handle errors. Update imports and remove unused routes.

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
-const userController = require("../controllers/users.js");
 
-router
-  .route("/signup")
-  .get(userController.renderSignupForm)
-  .post(wrapAsync(userController.signup));
+router.route("/signup")
+    .get((req, res) => {
+        res.render("users/signup.ejs");
+    })
+    .post(wrapAsync(async (req, res) => {
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ email, username });
+            const registeredUser = await User.register(newUser, password);
+            req.login(registeredUser, (err) => {
+                if (err) return next(err);
+                req.flash("success", "Welcome to Staycation!");
+                res.redirect("/listings");
+            });
+        } catch (e) {
+            req.flash("error", e.message);
+            res.redirect("/signup");
+        }
+    }));
 
-router
-  .route("/login")
-  .get(userController.renderLoginForm)
-  .post(
-    saveRedirectUrl,
-    passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureFlash: true,
-    }),
-    userController.login
-  );
-
-router.get("/logout", userController.logout);
-
+// Similar logic for /login...
 module.exports = router;
